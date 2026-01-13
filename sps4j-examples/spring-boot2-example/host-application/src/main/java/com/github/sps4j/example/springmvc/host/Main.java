d820b82046fa37259fb37b1fcc0194d7a2fb4031
秦利斌 <68638598+Allan-QLB@users.noreply.github.com>
2026-01-13 20:37:02 +0800

    fix doc (#15)

--- sps4j-examples/spring-boot2-example/host-application/src/main/java/com/github/sps4j/example/springmvc/host/Main.java
+++ sps4j-examples/spring-boot2-example/host-application/src/main/java/com/github/sps4j/example/springmvc/host/Main.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class Main {
     static {
         System.setProperty("spring.application.admin.enabled", "false");
     }
 
     public static void main(String[] args) {
-       SpringApplication.run(App.class, args);
+       SpringApplication.run(Main.class, args);
     }
 }
 
