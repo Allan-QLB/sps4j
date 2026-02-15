e0303f572acea936f26bc054e736ae4efa8b6e02
qchole <qin_libin@foxmail.com>
2026-02-16 07:49:23 +0800

    support jetty webserver

--- sps4j-spring-boot2/src/main/java/io/github/sps4j/springboot2/config/TomcatAutoConfiguration.java
+++ sps4j-spring-boot2/src/main/java/io/github/sps4j/springboot2/config/TomcatAutoConfiguration.java
@@ -1,36 +1,35 @@
 package io.github.sps4j.springboot2.config;
 
-import io.github.sps4j.springboot2.web.AddPluginServletContextPathCustomizer;
 import io.github.sps4j.springboot2.web.Sps4jTomcatWebServerFactory;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Auto configuration for SPS4J Spring MVC plugins.
- * <p>
- * This configuration is activated only when running inside an {@code Sps4jPluginClassLoader}
- * for a servlet web application. It provides a custom {@link ServletWebServerFactory}
- * to integrate the plugin's web server with the main application's Tomcat instance.
- *
- * @author Allan-QLB
- */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnExpression("T(java.lang.Thread).currentThread().contextClassLoader instanceof T(io.github.sps4j.core.load.Sps4jPluginClassLoader)")
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-@AutoConfigureBefore({ServletWebServerFactoryAutoConfiguration.class, JettyAutoConfiguration.class})
-public class Sps4jSpringMvcAutoConfiguration {
+@ConditionalOnClass(Tomcat.class)
+@AutoConfigureBefore({ServletWebServerFactoryAutoConfiguration.class})
+public class TomcatAutoConfiguration {
 
+    /**
+     * Creates a {@link Sps4jTomcatWebServerFactory} bean.
+     * <p>
+     * This factory is responsible for creating a {@link org.springframework.boot.web.server.WebServer}
+     * that delegates to the base application's Tomcat instance, allowing the plugin
+     * to deploy its own servlets and filters within the same server.
+     *
+     * @return The custom {@link ServletWebServerFactory}.
+     */
     @Bean
-    public AddPluginServletContextPathCustomizer addPluginServletContextPathCustomizer(ServerProperties serverProperties) {
-        return new AddPluginServletContextPathCustomizer(serverProperties);
+    public ServletWebServerFactory webServerFactory() {
+        return new Sps4jTomcatWebServerFactory(Thread.currentThread().getContextClassLoader());
     }
 
 }
