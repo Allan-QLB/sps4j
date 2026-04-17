068ce0f8873d68c88e8cb99c6dadadacd58042b1
qchole <qin_libin@foxmail.com>
2026-04-17 19:59:24 +0800

    add windows bat scripts for examples

---
+++ sps4j-examples/spring-boot3-example/host-application-jetty-spring-boot3/run.bat
@@ -0,0 +1,8 @@
+@echo off
+
+for /f "tokens=*" %%i in ('mvn help:evaluate -Dexpression=revision -q -DforceStdout') do set VERSION=%%i
+set SCRIPT_DIR=%~dp0
+set DIST_DIR=%SCRIPT_DIR%dist
+for %%i in ("%SCRIPT_DIR%.") do set PROJECT_NAME=%%~nxi
+
+java -cp "%DIST_DIR%\%PROJECT_NAME%-%VERSION%.jar;%DIST_DIR%\lib\*;%DIST_DIR%" host.Main %*
---
+++ sps4j-examples/spring-boot3-example/host-application-spring-boot3/run.bat
@@ -0,0 +1,8 @@
+@echo off
+
+for /f "tokens=*" %%i in ('mvn help:evaluate -Dexpression=revision -q -DforceStdout') do set VERSION=%%i
+set SCRIPT_DIR=%~dp0
+set DIST_DIR=%SCRIPT_DIR%dist
+for %%i in ("%SCRIPT_DIR%.") do set PROJECT_NAME=%%~nxi
+
+java -cp "%DIST_DIR%\%PROJECT_NAME%-%VERSION%.jar;%DIST_DIR%\lib\*;%DIST_DIR%" io.github.sps4j.example.springmvc.host.Main %*
